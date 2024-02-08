0a14b708d95d29f63d53e9c2ebd59b4aeca07aba
danielbui78 <danielbui78@gmail.com>
2024-02-08 10:23:17 -0500

    version update

--- include/common_version.h
+++ include/common_version.h
@@ -2,9 +2,9 @@
 #include "dzversion.h"
 
 // Version number for Common
-#define COMMON_MAJOR	2023
+#define COMMON_MAJOR	2024
 #define COMMON_MINOR	2
-#define COMMON_REV		2
-#define COMMON_BUILD	83
+#define COMMON_REV		3
+#define COMMON_BUILD	84
 
 #define COMMON_VERSION	DZ_MAKE_VERSION( COMMON_MAJOR, COMMON_MINOR, COMMON_REV, COMMON_BUILD )
