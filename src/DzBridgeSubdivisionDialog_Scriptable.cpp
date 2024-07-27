d65b871c11440d98049298e7dc329d579aa58a27
danielbui78 <danielbui78@gmail.com>
2024-07-27 10:14:04 -0400

    Stand alone UnitTest Scriptable plugin overhaul

--- src/DzBridgeSubdivisionDialog_Scriptable.cpp
+++
@@ -1,9 +0,0 @@
-
-#include "DzBridgeSubdivisionDialog_Scriptable.h"
-
-/*****************************
-Local definitions
-*****************************/
-#define DAZ_BRIDGE_LIBRARY_NAME "Daz Bridge"
-
-#include "moc_DzBridgeSubdivisionDialog_Scriptable.cpp"
