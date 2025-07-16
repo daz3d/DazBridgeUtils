65da43b44f72a15986076bfd8a1317da1dd1f830
danielbui78 <danielbui78@gmail.com>
2025-07-16 15:38:55 -0400

    Migrate ConvertRig, ConvertFbxJoints to base class

--- include/DzBridgeAction.h
+++ include/DzBridgeAction.h
@@ -390,6 +390,9 @@ namespace DzBridgeNameSpace
 		bool m_bOverrideMorphSelectionDialog = false; // NOTE: m_bOverrideMorphSelectionDialog is intended for Bridge Plugins that need to override the morph selection dialog in interactive mode, this is not needed for scripting modes
 
 		bool m_bBakeMeshesToSingleBindPose = true;
+		bool m_bConvertRig = true; // set to false to override base class Rig Conversion, see DzBridgeAction::preProcessScene()
+		bool m_bConvertFbxJoints = true; // set to false to override base class Joint Conversion operations, see DzBridgeAction::postProcessFbx()
+		QString m_sExportRigMode = "";
 		
 		////////////////////////////////////
 		
