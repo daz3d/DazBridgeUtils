56b05a9b55f251da7b5ba9e6e94913a78540bbfe
danielbui78 <danielbui78@gmail.com>
2025-12-10 10:14:14 -0800

    FIX clothing pelvis weightmaps

--- src/FbxTools.cpp
+++ src/FbxTools.cpp
@@ -5011,7 +5011,7 @@ FbxNode* FbxTools::ImportNode(QString sSourceNodeName, FbxNode* pDestinationPare
 	return pClonedNode;
 }
 
-bool ProjectWeights(FbxScene* pScene, FbxMesh* pMesh, FbxNode* pDestNode, FbxNode* pSourceNode, double fTransferWeight, double fAttenuationConstant)
+bool ProjectWeights(FbxScene* pScene, FbxMesh* pMesh, FbxNode* pDestNode, FbxNode* pSourceNode, double fTransferWeight, double fRadius, double fFallOff, QString sFilterMode="")
 {
 	if (!pScene || !pMesh || !pDestNode || !pSourceNode) return false;
 
@@ -5081,17 +5081,25 @@ bool ProjectWeights(FbxScene* pScene, FbxMesh* pMesh, FbxNode* pDestNode, FbxNod
 			// get vertex coordinate
 			FbxVector4 oVertexCoord = pVertexBuffer[nActualVertexIndex];
 			// calc distance
-			double fDistance = oDestinationCoord.Distance(oVertexCoord);
+			double fDistance = oDestinationCoord.Distance(oVertexCoord) / fRadius;
+			fDistance = FbxClamp(fDistance, 0.0, 1.0);
 			// calc attenuation factor
-			double fAttenuation = 1 / (1 + 0.22 * fDistance + fAttenuationConstant * fDistance * fDistance);
-			double fAttenuatedWeight = fAttenuation * fTransferWeight;
+			double fAttenuatedWeight = fTransferWeight * pow(1.0 - fDistance, fFallOff);
+			if (sFilterMode == "#right_only") {
+				if (oVertexCoord[0] > 0) fAttenuatedWeight = 0;
+			}
+			else if (sFilterMode == "#left_only") {
+				if (oVertexCoord[0] < 0) fAttenuatedWeight = 0;
+			}
+			else {
+				assert(sFilterMode == "");
+			}
 			double fWeightToTransfer = std::min(pSourceWeights[i], fAttenuatedWeight);
 			if (fTransferWeight == -1) {
 				fWeightToTransfer = pSourceWeights[i];
 			}
 			pSourceWeights[i] = std::max(0.0, pSourceWeights[i] - fWeightToTransfer);
 			oDestWeightMap[nActualVertexIndex] += fWeightToTransfer;
-
 		}
 	}
 
@@ -5107,6 +5115,125 @@ bool ProjectWeights(FbxScene* pScene, FbxMesh* pMesh, FbxNode* pDestNode, FbxNod
 }
 
 
+FbxVector4 GetNormalVector(FbxVector4 oPoint, FbxVector4 oLinePoint, FbxVector4 oLineDir)
+{
+	FbxVector4 oVectorA = oPoint - oLinePoint;
+	double fLengthSquared = oLineDir.DotProduct(oLineDir);
+	double fT = oVectorA.DotProduct(oLineDir) / fLengthSquared;
+	FbxVector4 oProjection = oLinePoint + oLineDir * fT;
+	FbxVector4 oNormalVector = oPoint - oProjection;
+	return oNormalVector;
+}
+
+bool RetargetCorrectiveWeights(FbxScene* pScene, FbxMesh* pMesh, FbxNode* pDestNode, FbxNode* pSourceNode, double fTransferWeight, FbxVector4 oReferencePoint, FbxVector4 oReferenceDir)
+{
+	if (!pScene || !pMesh || !pDestNode || !pSourceNode) return false;
+
+	FbxNode* pMeshNode = pMesh->GetNode();
+	if (!pMeshNode) return false;
+
+	FbxSkin* pSkin = nullptr;
+	const int nDef = pMesh->GetDeformerCount(FbxDeformer::eSkin);
+	for (int i = 0; i < nDef; ++i) {
+		FbxDeformer* pDef = pMesh->GetDeformer(i, FbxDeformer::eSkin);
+		if (pDef) { pSkin = FbxCast<FbxSkin>(pDef); break; }
+	}
+	if (!pSkin) { pSkin = FbxSkin::Create(pScene, "Skin"); pMesh->AddDeformer(pSkin); }
+
+	FbxCluster* pDestCluster = nullptr;
+	for (int i = 0; i < pSkin->GetClusterCount(); ++i) {
+		FbxCluster* pC = pSkin->GetCluster(i);
+		if (pC && pC->GetLink() == pDestNode) {
+			pDestCluster = pC;
+			break;
+		}
+	}
+	if (!pDestCluster) {
+		pDestCluster = FbxCluster::Create(pScene, (std::string("Cluster_") + pDestNode->GetName()).c_str());
+		pDestCluster->SetLink(pDestNode);
+		pDestCluster->SetLinkMode(FbxCluster::eNormalize); // normalize to 1.0
+		pSkin->AddCluster(pDestCluster);
+	}
+
+	int numSourceClusters = -1;
+	QList<FbxCluster*> aSourceClusters;
+	for (int i = 0; i < pSkin->GetClusterCount(); ++i) {
+		FbxCluster* pC = pSkin->GetCluster(i);
+		if (pC && pC->GetLink() == pSourceNode) {
+			aSourceClusters.append(pC);
+		}
+	}
+	numSourceClusters = aSourceClusters.length();
+
+	FbxAMatrix oMeshWsTransform = pMeshNode->EvaluateGlobalTransform();
+	FbxAMatrix oDestWsTransform = pDestNode->EvaluateGlobalTransform();
+	pDestCluster->SetTransformMatrix(oMeshWsTransform);
+	pDestCluster->SetTransformLinkMatrix(oDestWsTransform); // required for correct skinning
+
+	QMap<int, double> oDestWeightMap;
+	int numDestWeights = pDestCluster->GetControlPointIndicesCount();
+	const int* pDestVertexIndexBuffer = pDestCluster->GetControlPointIndices();
+	const double* pDestWeights = pDestCluster->GetControlPointWeights();
+
+	for (int i = 0; i < numDestWeights; i++)
+	{
+		int nActualVertexIndex = pDestVertexIndexBuffer[i];
+		double fDestWeight = pDestWeights[i];
+		oDestWeightMap.insert(nActualVertexIndex, fDestWeight);
+	}
+
+	FbxVector4 oDestinationCoord = oDestWsTransform.GetT();
+	FbxVector4 oDestVector = GetNormalVector(oDestinationCoord, oReferencePoint, oReferenceDir);
+	oDestVector.Normalize();
+	FbxVector4* pVertexBuffer = pMesh->GetControlPoints();
+	foreach(FbxCluster * pSourceCluster, aSourceClusters)
+	{
+		const int numSourceVertices = pSourceCluster->GetControlPointIndicesCount();
+		int* pSourceVertexIndexBuffer = pSourceCluster->GetControlPointIndices();
+		double* pSourceWeights = pSourceCluster->GetControlPointWeights();
+		for (int i = 0; i < numSourceVertices; ++i)
+		{
+			int nActualVertexIndex = pSourceVertexIndexBuffer[i];
+			// get vertex coordinate
+			FbxVector4 oVertexCoord = pVertexBuffer[nActualVertexIndex];
+			FbxVector4 oVertexVector = GetNormalVector(oVertexCoord, oReferencePoint, oReferenceDir);
+			oVertexVector.Normalize();
+			// dot product
+			double fDotProduct =  oDestVector.DotProduct(oVertexVector);
+			double fAttenuatedWeight = fTransferWeight;
+			if (abs(fDotProduct) < DZ_DBL_EPSILON * 2) {
+				// orthoganal
+				fAttenuatedWeight = 0.0;
+			}
+			else if (fDotProduct > 0) {
+				// same direction
+				fAttenuatedWeight = fDotProduct * fTransferWeight;
+			}
+			else {
+				// opposite direction
+				fAttenuatedWeight = 0.0;
+			}
+			double fWeightToTransfer = std::min(pSourceWeights[i], fAttenuatedWeight);
+			if (fTransferWeight == -1) {
+				fWeightToTransfer = pSourceWeights[i];
+			}
+			pSourceWeights[i] = std::max(0.0, pSourceWeights[i] - fWeightToTransfer);
+			oDestWeightMap[nActualVertexIndex] += fWeightToTransfer;
+		}
+	}
+
+	pDestCluster->SetControlPointIWCount(0);
+
+	foreach(int nActualVertexIndex, oDestWeightMap.keys())
+	{
+		double fDestWeight = oDestWeightMap[nActualVertexIndex];
+		pDestCluster->AddControlPointIndex(nActualVertexIndex, fDestWeight);
+	}
+
+	return true;
+}
+
+#define POS(a) a->EvaluateGlobalTransform().GetT()
 bool FbxTools::AddMetahumanCorrectiveBones(FbxScene* pScene, FbxNode* pRootBone, QString sFbxCorrectiveFile)
 {
 	OpenFBXInterface* openFBX = OpenFBXInterface::GetInterface();
@@ -5116,14 +5243,26 @@ bool FbxTools::AddMetahumanCorrectiveBones(FbxScene* pScene, FbxNode* pRootBone,
 		return false;
 	}
 
+	FbxNode* pPelvis = pRootBone->FindChild("pelvis");
+
 	FbxNode* pThighL = pRootBone->FindChild("thigh_l");
 	FbxNode* pThighR = pRootBone->FindChild("thigh_r");
 	FbxNode* pCalfL = pRootBone->FindChild("calf_l");
 	FbxNode* pCalfR = pRootBone->FindChild("calf_r");
+	FbxNode* pFootL = pRootBone->FindChild("foot_l");
+	FbxNode* pFootR = pRootBone->FindChild("foot_r");
+
+	FbxNode* pThighTwist1L = pRootBone->FindChild("thigh_twist_01_l");
+	FbxNode* pThighTwist2L = pRootBone->FindChild("thigh_twist_02_l");
+	FbxNode* pThighTwist1R = pRootBone->FindChild("thigh_twist_01_r");
+	FbxNode* pThighTwist2R = pRootBone->FindChild("thigh_twist_02_r");
+
 	FbxNode* pUpperArmL = pRootBone->FindChild("upperarm_l");
 	FbxNode* pUpperArmR = pRootBone->FindChild("upperarm_r");
 	FbxNode* pLowerArmL = pRootBone->FindChild("lowerarm_l");
 	FbxNode* pLowerArmR = pRootBone->FindChild("lowerarm_r");
+	FbxNode* pHandL = pRootBone->FindChild("hand_l");
+	FbxNode* pHandR = pRootBone->FindChild("hand_r");
 
 	FbxNode* pThighCorrectiveL = ImportNode("thigh_correctiveRoot_l", pThighL, pImportScene, pScene, pRootBone);
 	FbxNode* pThighCorrectiveR = ImportNode("thigh_correctiveRoot_r", pThighR, pImportScene, pScene, pRootBone);
@@ -5150,28 +5289,88 @@ bool FbxTools::AddMetahumanCorrectiveBones(FbxScene* pScene, FbxNode* pRootBone,
 	foreach(FbxNode* pNode, aMeshList)
 	{
 		FbxMesh* pMesh = pNode->GetMesh();
+		FbxNode* pSourceBone;
 
-		FbxNode* pPelvis = pRootBone->FindChild("pelvis");
+		// Organizational Setup and Assumptions
+		if (pThighL && pThighR) {
+			TransferWeights(pScene, pMesh, pThighTwist1L, pThighL, 1.0);
+			TransferWeights(pScene, pMesh, pThighTwist1R, pThighR, 1.0);
+		}
+
 		if (pPelvis) {
-			int nNumCorrectiveBones = pThighCorrectiveL->GetChildCount();
-			double fWeights = 0.75 / nNumCorrectiveBones;
-			for (int i = 0; i < nNumCorrectiveBones; i++)
-			{
-				FbxNode* pCorrective = pThighCorrectiveL->GetChild(i);
-				//ProjectWeights(pScene, pMesh, pCorrective, pPelvis, 0.4, 0.05);
-			}
+			pSourceBone = pPelvis;
+
+			// RESHAPE PELVIS WEIGHT MAP
+			ProjectWeights(pScene, pMesh, pThighTwist1L, pSourceBone, 0.7, 30, 2.5, "#left_only");
+			ProjectWeights(pScene, pMesh, pThighTwist1R, pSourceBone, 0.7, 30, 2.5, "#right_only");
+
+			FbxNode* pCorrective;
+			pCorrective = pRootBone->FindChild("thigh_fwd_lwr_l");
+			if (pCorrective) ProjectWeights(pScene, pMesh, pCorrective, pSourceBone, 0.3, 24, 3, "#left_only");
+			pCorrective = pRootBone->FindChild("thigh_fwd_lwr_r");
+			if (pCorrective) ProjectWeights(pScene, pMesh, pCorrective, pSourceBone, 0.3, 24, 3, "#right_only");
+
+			pCorrective = pRootBone->FindChild("thigh_fwd_l");
+			if (pCorrective) ProjectWeights(pScene, pMesh, pCorrective, pSourceBone, 0.3, 24, 3, "#left_only");
+			pCorrective = pRootBone->FindChild("thigh_fwd_r");
+			if (pCorrective) ProjectWeights(pScene, pMesh, pCorrective, pSourceBone, 0.3, 24, 3, "#right_only");
+
+			pCorrective = pRootBone->FindChild("thigh_bck_lwr_l");
+			if (pCorrective) ProjectWeights(pScene, pMesh, pCorrective, pSourceBone, 0.7, 35, 2, "#left_only");
+			pCorrective = pRootBone->FindChild("thigh_bck_lwr_r");
+			if (pCorrective) ProjectWeights(pScene, pMesh, pCorrective, pSourceBone, 0.7, 35, 2, "#right_only");
+
+			pCorrective = pRootBone->FindChild("thigh_bck_l");
+			if (pCorrective) ProjectWeights(pScene, pMesh, pCorrective, pSourceBone, 0.5, 25, 2, "#left_only");
+			pCorrective = pRootBone->FindChild("thigh_bck_r");
+			if (pCorrective) ProjectWeights(pScene, pMesh, pCorrective, pSourceBone, 0.5, 25, 2, "#right_only");
+
+			pCorrective = pRootBone->FindChild("thigh_out_l");
+			if (pCorrective) ProjectWeights(pScene, pMesh, pCorrective, pSourceBone, 0.5, 24, 2, "#left_only");
+			pCorrective = pRootBone->FindChild("thigh_out_r");
+			if (pCorrective) ProjectWeights(pScene, pMesh, pCorrective, pSourceBone, 0.5, 24, 2, "#right_only");
+
+			pCorrective = pRootBone->FindChild("thigh_in_l");
+			if (pCorrective) ProjectWeights(pScene, pMesh, pCorrective, pSourceBone, 0.5, 24, 2, "#left_only");
+			pCorrective = pRootBone->FindChild("thigh_in_r");
+			if (pCorrective) ProjectWeights(pScene, pMesh, pCorrective, pSourceBone, 0.5, 24, 2, "#right_only");
+
 		}
 
-		FbxNode* pThighTwist01L = pRootBone->FindChild("thigh_twist_01_l");
-		if (pThighTwist01L) {
-			int nNumCorrectiveBones = pThighCorrectiveL->GetChildCount();
-			double fWeights = 0.75 / nNumCorrectiveBones;
-			for (int i = 0; i < nNumCorrectiveBones; i++)
-			{
-				FbxNode* pCorrective = pThighCorrectiveL->GetChild(i);
-				//ProjectWeights(pScene, pMesh, pCorrective, pThighTwist01L, 0.8, 0.04);
-			}
+#if 0
+		pSourceBone = pRootBone->FindChild("thigh_twist_01_l");
+		if (pSourceBone) {
+			FbxVector4 oReferencePoint = POS(pThighL);
+			FbxVector4 oReferenceDir = POS(pCalfL) - POS(pThighL);
+			FbxNode* pCorrective = pRootBone->FindChild("thigh_in_l");
+			if (pCorrective) RetargetCorrectiveWeights(pScene, pMesh, pCorrective, pSourceBone, 1.0, oReferencePoint, oReferenceDir);
+
+			pCorrective = pRootBone->FindChild("thigh_fwd_lwr_l");
+			if (pCorrective) RetargetCorrectiveWeights(pScene, pMesh, pCorrective, pSourceBone, 1.0, oReferencePoint, oReferenceDir);
+
+			pCorrective = pRootBone->FindChild("thigh_bck_lwr_l");
+			if (pCorrective) RetargetCorrectiveWeights(pScene, pMesh, pCorrective, pSourceBone, 1.0, oReferencePoint, oReferenceDir);
+
+			pCorrective = pRootBone->FindChild("thigh_out_l");
+			if (pCorrective) RetargetCorrectiveWeights(pScene, pMesh, pCorrective, pSourceBone, 1.0, oReferencePoint, oReferenceDir);
 		}
+
+		pSourceBone = pRootBone->FindChild("thigh_twist_01_r");
+		if (pSourceBone) {
+			FbxVector4 oReferencePoint = POS(pThighR);
+			FbxVector4 oReferenceDir = POS(pCalfR) - POS(pThighR);
+			FbxNode* pCorrective = pRootBone->FindChild("thigh_in_r");
+			if (pCorrective) RetargetCorrectiveWeights(pScene, pMesh, pCorrective, pSourceBone, 1.0, oReferencePoint, oReferenceDir);
+
+			pCorrective = pRootBone->FindChild("thigh_fwd_lwr_r");
+			if (pCorrective) RetargetCorrectiveWeights(pScene, pMesh, pCorrective, pSourceBone, 1.0, oReferencePoint, oReferenceDir);
+			pCorrective = pRootBone->FindChild("thigh_bck_lwr_r");
+			if (pCorrective) RetargetCorrectiveWeights(pScene, pMesh, pCorrective, pSourceBone, 1.0, oReferencePoint, oReferenceDir);
+
+			pCorrective = pRootBone->FindChild("thigh_out_r");
+			if (pCorrective) RetargetCorrectiveWeights(pScene, pMesh, pCorrective, pSourceBone, 1.0, oReferencePoint, oReferenceDir);
+		}
+#endif
 
 	}
 
